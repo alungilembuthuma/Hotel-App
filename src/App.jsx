2d8af5aaae80b5c98146632f7b93a518c1596c30
alungilembuthuma <alungilembuthuma0320@gmail.com>
2024-10-16 09:18:28 +0200

    added routes and pages

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import Landing from './Pages/Landing';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import AccommodationList from './Pages/AccomodationList'
+import AccommodationDetails from './Pages/AccomodationDetails';
+import Booking from './Pages/Booking';
+import UserProfile from './Pages/UserProfile';
+import AdminLogin from './Pages/AdminLogin';
+import AdminDashboard from './Pages/AdminDashboard';
+import AddAccommodation from './Pages/AddAccomodation';
+import SearchResults from './Pages/SearchResults';
+import Favorites from './Pages/Favorites';
+import Reviews from './Pages/Reviews';
+// import Notifications from './Pages/';
+import PayGateWay from './Pages/PayGateWay';
+import AdminRegister from './Pages/AdminRegister';
+import BookAccomodation from './Pages/BookAccomodation';
+
+function App() {
+  return (
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/accommodations" element={<AccommodationList />} />
+        <Route path="/accommodation/:id" element={<AccommodationDetails />} />
+        <Route path="/booking/:id" element={<Booking />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/accommodations/new" element={<AddAccommodation />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/reviews/:id" element={<Reviews />} />
+        {/* <Route path="/notifications" element={<Notifications />} /> */}
+        <Route path="/pay-gateway" element={<PayGateWay />} />
+        <Route path="/admin/register" element={<AdminRegister />} />
+        <Route path="/BookAccomodation" element={<BookAccomodation />} />
+      </Routes>
+    </BrowserRouter>     );
+}
+
+export default App;
